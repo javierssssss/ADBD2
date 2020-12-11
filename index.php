64a5e498d02a938dc479f741a22cdbae8bf4e3e1
javierssssss <66440591+javierssssss@users.noreply.github.com>
2020-12-11 01:22:12 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+session_status();
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    
+    <!-- Title Page -->  
+    <title>Login</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="estilos.css"> 
+</head>
+<body>
+    <div id="container">
+    <?php       if (!isset($_SESSION['conectado'])    ): ?>
+        <form method="post" action="submit.php">
+            <!-- Username -->
+            <label for="name">Usuario:</label>
+            <input  type="name" name="usuario" required>
+            <!-- Password -->
+            <label for="username">Clave:</label>
+           
+            <input required type="password" name="clave" required>
+            <div id="lower">
+                <!-- Submit Button -->
+                <input type="submit" value="Login">
+            </div>
+        </form>    
+        <?php endif; ?>   
+        <?php if ( isset($_SESSION['conectado']) && $_SESSION["conectado"]=="C" ): ?>
+            <p>Login con exito !</p>
+          <form  action="salir.php" method="post" >
+              <input type="hidden" name="salir" value="salir">
+            <button type="submit">Salir</button>
+
+        </form>  
+        <?php endif; ?>   
+        
+    </div>
+</body>
+</html>
