b0dff2207e374a88d77c7d51bf8369ed1a0431ae
javierssssss <66440591+javierssssss@users.noreply.github.com>
2020-12-11 12:20:53 -0500

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,8 +1,8 @@
 <?php
 try{
     session_start();
-    $un = $_POST['usuario'];
-    $pwd = $_POST['clave'];
+    $un = $_GET['usuario'];
+    $pwd = $_GET['clave'];
 
    /* if(empty($un) || empty($pwd)){
         echo"<script>alert('Debe Llenar los campos vacios');window.location.href='index.php';</script>";
@@ -30,38 +30,7 @@ try{
                    // $resultado->close();
                 }
             }
-    
-  
-    
-    $bdd = new PDO(
-        "mysql:host=" . getenv("MYSQL_ADDON_HOST") . ";dbname=" . getenv("MYSQL_ADDON_DB"),
-        getenv("MYSQL_ADDON_USER"),
-        getenv("MYSQL_ADDON_PASSWORD")
-    );
-    if ( isset($bdd) ) {
-        $sql="SELECT * FROM users WHERE usuario=".$un;//."' AND clave = '".$pwd."'";
-        //echo $sql;
-        //$stmt = $bdd->query($sql);
-        /*$stmt->bindValue(':un', 'Joe');
-        $stmt->bindValue(':pwd', 'Joe');*/
-
-        
-       // $result = $stmt->fetch();
-        //echo count($result) ;
-       // if (count($result) == 1){
-        /*    echo "Logueado";
-            session_status();
-            $_SESSION["conectado"]="C";
-            //header('Location: https://sql-injection-insecure.cleverapps.io/ ');
-        }else{
-           // header('Location: https://sql-injection-insecure.cleverapps.io/ ');
-        }*/
-       
-    
-    }else{
-        die("Connection failed: " . $conn->connect_error);
-        header('Location: https://sql-injection-insecure.cleverapps.io/ ');
-    }
+ 
 }catch(Exception $e){
     echo $e->getMessage();
 }
